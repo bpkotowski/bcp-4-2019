643bdd504895c11a1a370a018686e04bb8000f8d
bpkotowski <bighoopee@gmail.com>
2019-01-17 19:46:43 -0800

    more code added

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 function greetMe() {
     console.log('greet function called');
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
+    
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value; 
+
+    var greeting = 'Hello'  + firstName + lastName + '!';
+
+    console.log(greeting);
+
+    console.log(firstName,  lastName);
 
 
 }
