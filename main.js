570646e2943e05d9c1103c2ef65aaeaad6695c20
bpkotowski <bighoopee@gmail.com>
2019-01-17 20:04:49 -0800

    it works!

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 function greetMe() {
     console.log('greet function called');
+
     var firstNameInput = document.getElementById('first-name');
     var lastNameInput = document.getElementById('last-name');
     
     var firstName = firstNameInput.value;
     var lastName = lastNameInput.value; 
+    
+    console.log(firstNameInput.value);
 
-    var greeting = 'Hello'  + firstName + lastName + '!';
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
+
+    var greeter = document.getElementById('greeter');
+    greeter.textContent = greeting;
 
     console.log(greeting);
 
-    console.log(firstName,  lastName);
+    console.log(firstName, lastName);
 
 
 }
